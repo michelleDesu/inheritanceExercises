3e9640784190dfe8859e7bc0ed7d25909ac2d873
michelle <michelle.johansson85@gmail.com>
2020-05-17 16:50:51 +0200

    IceCream.java: * added class for IceCream

---
+++ src/main/java/se/lexicon/michelle/iceCream/IceCream.java
@@ -0,0 +1,11 @@
+package se.lexicon.michelle.iceCream;
+
+public class IceCream {
+    public IceCream() {
+        System.out.println("Ice Cream");
+    }
+
+    public void description(){
+        System.out.println("I love Ice Cream");
+    }
+}
