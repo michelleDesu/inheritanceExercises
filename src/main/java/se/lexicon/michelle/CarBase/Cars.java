8ddd9f75bebaeebdcc78ad94bd32e3ce00d3a083
michelle <michelle.johansson85@gmail.com>
2020-05-17 17:31:27 +0200

    Cars.java * added comment

--- src/main/java/se/lexicon/michelle/CarBase/Cars.java
+++ src/main/java/se/lexicon/michelle/CarBase/Cars.java
@@ -1,5 +1,9 @@
 package se.lexicon.michelle.CarBase;
 
+/*
+    renamed class from Car to Cars to be able to have the classes int hte same project,
+    this due to Car already existing in the Vehicle package.
+ */
 public class Cars extends CarBase implements CarColour{
 
     public Cars(String carName) {
